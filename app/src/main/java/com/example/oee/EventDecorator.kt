b4e9eab6abdf226ceac12ad97f6569e247416482
Rachel <cofla226@naver.com>
2023-12-10 06:18:42 +0900

    EventDecorator.kt 파일 추가하여 운동한 날에 캘린더에 decoration추가

--- app/src/main/java/com/example/oee/EventDecorator.kt
+++ app/src/main/java/com/example/oee/EventDecorator.kt
@@ -1,4 +1,18 @@
 package com.example.oee
 
-class EventDecorator {
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import android.graphics.Color
+
+class EventDecorator(private val dates: Collection<CalendarDay>) : DayViewDecorator {
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        return dates.contains(day)
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(DotSpan(5f, Color.RED)) // 예시로 빨간색 사용
+    }
 }
+
