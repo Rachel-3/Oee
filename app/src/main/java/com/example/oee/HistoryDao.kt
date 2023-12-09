c2939e900895ad9deb9e438de7ca2583b8b2698a
Rachel <cofla226@naver.com>
2023-12-10 03:49:52 +0900

    HistoryDao.kt 파일 추석 설명 추가

--- app/src/main/java/com/example/oee/HistoryDao.kt
+++ app/src/main/java/com/example/oee/HistoryDao.kt
@@ -5,13 +5,15 @@ import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-
+// Room 데이터베이스를 위한 데이터 액세스 객체 인터페이스
 @Dao
 interface HistoryDao {
 
+    // 운동 기록을 데이터베이스에 삽입하는 함수
     @Insert
     suspend fun insert(historyEntity: HistoryEntity)
 
+    // 모든 운동 기록을 가져오는 쿼리 함수
     @Query("Select * from `history-table`")
     fun fetchALlDates():Flow<List<HistoryEntity>>
 }
