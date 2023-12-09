b52937067cb0df14f901a9ed53b50025740bd5c3
Rachel <cofla226@naver.com>
2023-12-10 06:11:47 +0900

    HistoryEntity yser, month, day추가

--- app/src/main/java/com/example/oee/HistoryEntity.kt
+++ app/src/main/java/com/example/oee/HistoryEntity.kt
@@ -6,7 +6,9 @@ import androidx.room.PrimaryKey
 // 운동 기록을 저장하는 Room 데이터베이스의 엔티티 클래스
 @Entity(tableName = "history-table")
 data class HistoryEntity(
-    // 기록 날짜를 기본 키로 사용
-    @PrimaryKey
-    val date:String
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val date: String
 )
