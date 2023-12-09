1ba9476037df3ed2ad1297fa2b26d57a6dcf93bd
Rachel <cofla226@naver.com>
2023-12-10 03:54:25 +0900

    WorkOutApp.kt 파일 추석 설명 추가

--- app/src/main/java/com/example/oee/WorkOutApp.kt
+++ app/src/main/java/com/example/oee/WorkOutApp.kt
@@ -2,9 +2,12 @@ package com.example.oee;
 
 import android.app.Application
 
+// 전체 애플리케이션에 걸쳐 사용되는 글로벌 애플리케이션 클래스
 class WorkOutApp: Application() {
 
-    val db:HistoryDatabase by lazy {
+    // 지연 초기화를 사용하여 HistoryDatabase의 인스턴스를 생성
+    // 애플리케이션 컨텍스트가 필요할 때만 HistoryDatabase의 인스턴스를 생성
+    val db: HistoryDatabase by lazy {
         HistoryDatabase.getInstance(this)
     }
 }
