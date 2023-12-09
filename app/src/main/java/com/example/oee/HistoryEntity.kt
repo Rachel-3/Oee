b0f1c46cc39decff13d3eacb4edb23fd97325142
Rachel <cofla226@naver.com>
2023-12-10 03:52:40 +0900

    HistoryEntity.kt 파일 추석 설명 추가

--- app/src/main/java/com/example/oee/HistoryEntity.kt
+++ app/src/main/java/com/example/oee/HistoryEntity.kt
@@ -3,7 +3,7 @@ package com.example.oee
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// 운동 기록을 저장하는 엔티티 클래스
+// 운동 기록을 저장하는 Room 데이터베이스의 엔티티 클래스
 @Entity(tableName = "history-table")
 data class HistoryEntity(
     // 기록 날짜를 기본 키로 사용
