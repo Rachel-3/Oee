b77ed09a081e9a813c81aec2bb2c97b2380280eb
kimsongie12 <song3078@naver.com>
2023-12-11 14:03:18 +0900

    BMI.kt파일 us계산기 빼고 수정

--- app/src/main/java/com/example/oee/BMIActivity.kt
+++ app/src/main/java/com/example/oee/BMIActivity.kt
@@ -10,61 +10,47 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
-    // 사용자 BMI 계산 액티비티
 
-    // Metric과 US 단위 계산에 사용될 상수를 정의
     companion object {
         private const val METRIC_UNITS_VIEW = "METRIC_UNIT_VIEW"
-        private const val US_UNITS_VIEW = "US_UNIT_VIEW"
+
     }
 
-    // 현재 보이는 뷰의 타입을 저장
     private var currentVisibleView: String =
         METRIC_UNITS_VIEW
 
-    // 뷰 바인딩을 위한 변수를 선언
     private var binding: ActivityBmiBinding? = null
-
-    // 액티비티가 생성될 때 호출
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 레이아웃 초기화
         binding = ActivityBmiBinding.inflate(layoutInflater)
 
         setContentView(binding?.root)
 
-        // 툴바 설정
         setSupportActionBar(binding?.toolbarBmiActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "CALCULATE BMI"
-
-        // 툴바의 뒤로 가기 버튼에 대한 클릭 리스너 설정
         binding?.toolbarBmiActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        // 기본적으로 메트릭 단위 뷰를 보이게 설정
         makeVisibleMetricUnitsView()
 
-        // 단위 변경 라디오 버튼 그룹에 리스너를 설정
         binding?.rgUnits?.setOnCheckedChangeListener { _, checkedId: Int ->
 
             if (checkedId == R.id.rbMetricUnits) {
                 makeVisibleMetricUnitsView()
             } else {
-                makeVisibleUsUnitsView()
+
             }
         }
 
-        // 계산 버튼에 클릭 리스너 설정
         binding?.btnCalculateUnits?.setOnClickListener {
             calculateUnits()
         }
 
     }
 
-    // 단위를 계산하는 함수
     private fun calculateUnits(){
 
         if (currentVisibleView == METRIC_UNITS_VIEW) {
@@ -88,43 +74,17 @@ class BMIActivity : AppCompatActivity() {
             }
         } else {
 
-            if (validateUsUnits()) {
 
-                val usUnitHeightValueFeet: String =
-                    binding?.etUsMetricUnitHeightFeet?.text.toString()
-                val usUnitHeightValueInch: String =
-                    binding?.etUsMetricUnitHeightInch?.text.toString()
-                val usUnitWeightValue: Float = binding?.etUsMetricUnitWeight?.text.toString()
-                    .toFloat()
-
-
-                val heightValue =
-                    usUnitHeightValueInch.toFloat() + usUnitHeightValueFeet.toFloat() * 12
-
-
-                val bmi = 703 * (usUnitWeightValue / (heightValue * heightValue))
-
-                displayBMIResult(bmi)
-            } else {
-                Toast.makeText(
-                    this@BMIActivity,
-                    "Please enter valid values.",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
-            }
         }
     }
 
 
-    // 메트릭 단위 입력 폼을 보이게 하는 함수
     private fun makeVisibleMetricUnitsView() {
         currentVisibleView = METRIC_UNITS_VIEW
         binding?.tilMetricUnitWeight?.visibility = View.VISIBLE
         binding?.tilMetricUnitHeight?.visibility = View.VISIBLE
         binding?.tilUsMetricUnitWeight?.visibility = View.GONE
-        binding?.tilMetricUsUnitHeightFeet?.visibility = View.GONE
-        binding?.tilMetricUsUnitHeightInch?.visibility = View.GONE
+
 
         binding?.etMetricUnitHeight?.text!!.clear()
         binding?.etMetricUnitWeight?.text!!.clear()
@@ -133,23 +93,9 @@ class BMIActivity : AppCompatActivity() {
     }
 
 
-    // 미국 단위 입력 폼을 보이게 하는 함수
-    private fun makeVisibleUsUnitsView() {
-        currentVisibleView = US_UNITS_VIEW
-        binding?.tilMetricUnitHeight?.visibility = View.INVISIBLE
-        binding?.tilMetricUnitWeight?.visibility = View.INVISIBLE
-        binding?.tilUsMetricUnitWeight?.visibility = View.VISIBLE
-        binding?.tilMetricUsUnitHeightFeet?.visibility = View.VISIBLE
-        binding?.tilMetricUsUnitHeightInch?.visibility = View.VISIBLE
-
-        binding?.etUsMetricUnitWeight?.text!!.clear()
-        binding?.etUsMetricUnitHeightFeet?.text!!.clear()
-        binding?.etUsMetricUnitHeightInch?.text!!.clear()
-        binding?.llDiplayBMIResult?.visibility = View.INVISIBLE
-    }
 
 
-    // 메트릭 단위 입력값을 검증하는 함수
+
     private fun validateMetricUnits(): Boolean {
         var isValid = true
 
@@ -163,65 +109,55 @@ class BMIActivity : AppCompatActivity() {
     }
 
 
-    // 미국 단위 입력값을 검증하는 함수
     private fun validateUsUnits(): Boolean {
         var isValid = true
 
-        when {
-            binding?.etUsMetricUnitWeight?.text.toString().isEmpty() -> {
-                isValid = false
-            }
-            binding?.etUsMetricUnitHeightFeet?.text.toString().isEmpty() -> {
-                isValid = false
-            }
-            binding?.etUsMetricUnitHeightInch?.text.toString().isEmpty() -> {
-                isValid = false
-            }
-        }
+
+
+
 
         return isValid
     }
 
 
-    // BMI 결과를 표시하는 함수
     private fun displayBMIResult(bmi: Float) {
 
         val bmiLabel: String
         val bmiDescription: String
 
         if (bmi.compareTo(15f) <= 0) {
-            bmiLabel = "Very severely underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+            bmiLabel = "매우 심각한 저체중"
+            bmiDescription = "헉! 당신을 더 잘 돌봐야할거같아요. 더 많이 먹어야해요 !!"
         } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0
         ) {
-            bmiLabel = "Severely underweight"
-            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+            bmiLabel = "심각한 저체중"
+            bmiDescription = "아! 정말 당신의 건강을 더 신경쓰세요 더 많이 먹어요 !!"
         } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0
         ) {
-            bmiLabel = "Underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+            bmiLabel = "저체중"
+            bmiDescription = "이제 좀 더 노력하면돼요!! 꾸준히 먹어요 !!"
         } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0
         ) {
-            bmiLabel = "Normal"
-            bmiDescription = "Congratulations! You are in a good shape!"
+            bmiLabel = "정상"
+            bmiDescription = "축하해요! 당신은 좋은 체형을 유지하고 있어요!"
         } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(
                 bmi,
                 30f
             ) <= 0
         ) {
-            bmiLabel = "Overweight"
-            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+            bmiLabel = "과체중"
+            bmiDescription = "건강에 조금 더 신경을 써야할 거 같아요. 운동을 하세요"
         } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0
         ) {
-            bmiLabel = "Obese Class | (Moderately obese)"
-            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+            bmiLabel = "비만 정도 | (적당한 비만)"
+            bmiDescription = "자기 건강에 신경을 써야해요. 운동을 해야할 거 같아요!"
         } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0
         ) {
-            bmiLabel = "Obese Class || (Severely obese)"
-            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+            bmiLabel = "비만 정도 || (심각한 비만)"
+            bmiDescription = "당신은 매우 위험한 몸이에요 !! 지금 행동하세요"
         } else {
-            bmiLabel = "Obese Class ||| (Very Severely obese)"
-            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+            bmiLabel = "비만 정도 ||| (매우 심각한 비만)"
+            bmiDescription = "당신은 매우 위험한 상태에 있어요!! 제발 지금 당장 운동하세요"
         }
 
 
@@ -237,7 +173,6 @@ class BMIActivity : AppCompatActivity() {
     // END
 
 
-    // 액티비티가 소멸될 때 호출되는 함수
     override fun onDestroy() {
         super.onDestroy()
         binding = null
